82b493884d883652557d280f64013690ae748dce
roushou <roushou9@gmail.com>
2024-09-04 15:09:58 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wagen
+
+## Getting started
+
+Install dependencies.
+
+```sh
+$ bun install
+```
+
+Launch the development server.
+
+```sh
+$ bun dev
+```
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/_health", (ctx) => {
+  return ctx.text("healthy");
+});
+
+export default app;
